b61a2262a4f7f69bc188a23cf723bfac89ae41a0
Bobby Hirtle <protonbobby@gmail.com>
2020-11-22 09:40:56 -0500

    Don't purge tailwind to maintain all classes in web

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: { content: [] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
